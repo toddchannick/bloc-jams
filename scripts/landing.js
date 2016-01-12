a30872e1d1a61e234264df17cd78d41217b6afe1
Todd Channick <toc5012@gmail.com>
2016-01-12 16:36:50 -0500

    Added animation on scroll

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,19 @@
-var animatePoints = function () {
+var scrollFn = (function () {
 
-  var points = document.getElementsByClassName('point');
+  return function (event) {
+    var points = document.getElementsByClassName('point');
 
-  var revealFirstPoint = function () {
-    points[0].style.opacity = 1;
-    points[0].style.transform = "scaleX(1) translateY(0)";
-    points[0].style.msTransform = "scaleX(1) translateY(0)";
-    points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+    var revealPoint = function (ptIndex) {
+      points[ptIndex].style.opacity = 1;
+      points[ptIndex].style.transform = "scaleX(1) translateX(0)";
+      points[ptIndex].style.msTransform = "scaleX(1) translateX(0)";
+      points[ptIndex].style.WebkitTransform = "scaleX(1) translateX(0)";
+    }
 
-  var revealSecondPoint = function () {
-    points[1].style.opacity = 1;
-    points[1].style.transform = "scaleX(1) translateY(0)";
-    points[1].style.msTransform = "scaleX(1) translateY(0)";
-    points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+    for (var i = 0; i < points.length; i++) {
+      revealPoint(i);
+    }
+  }
+})();
 
-  var revealThirdPoint = function () {
-    points[2].style.opacity = 1;
-    points[2].style.transform = "scaleX(1) translateY(0)";
-    points[2].style.msTransform = "scaleX(1) translateY(0)";
-    points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
-
-  revealFirstPoint();
-  revealSecondPoint();
-  revealThirdPoint();
-
-};
+window.addEventListener('scroll', scrollFn);
