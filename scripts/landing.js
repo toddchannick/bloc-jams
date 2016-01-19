1b52f2a4f3f59668dae378c206eb904cd9659327
Todd Channick <toc5012@gmail.com>
2016-01-19 14:15:57 -0500

    Checkpoint 29 Complete - Refactored Landing Page with jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,28 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function () {
+  
+  var revealPoint = function () {
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
 
-var animatePoints = function (points) {
+  $.each($('.point'), revealPoint);
 
-  var points = document.getElementsByClassName('point');
-
-  var revealPoint = function (ptIndex) {
-    points[ptIndex].style.opacity = 1;
-    points[ptIndex].style.transform = "scaleX(1) translateX(0)";
-    points[ptIndex].style.msTransform = "scaleX(1) translateX(0)";
-    points[ptIndex].style.WebkitTransform = "scaleX(1) translateX(0)";
-  }
-
-  for (var i = 0; i < points.length; i++) {
-    revealPoint(i);
-  }
 };
 
-window.onload = function () {
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+
+$(window).load(function () {
+  if ($(window).height() > 950) {
+    animatePoints();
   }
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener("scroll", function (event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+  $(window).scroll(function (event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
   });
-}
+
+});
